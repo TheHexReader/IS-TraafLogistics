6d9208f5e76b8f049c067627913de0009df55c9b
Hex <thehexreader@gmail.com>
2022-12-16 20:46:02 +0300

    Semi made users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,19 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+
+    if Role.find_by(id: current_user[:roles_id]).can_destroy
+      @user.destroy
+      redirect_to users_path
+    else
+      redirect_to root_path, status: :unauthorized
+    end
+  end
+
+  def edit
+    # To do
+  end
 end
